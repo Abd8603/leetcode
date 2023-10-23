7aa039e1be7fe57fc0619eaabb78681af8b8145f
Abd8603 <80663656+Abd8603@users.noreply.github.com>
2023-10-23 07:32:34 +0530

    Time: 1 ms (76.31%), Space: 39.3 MB (83.82%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,13 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n == 1) return true;
-        if(n < 1) return false;
-        return solve(n);
-    }
-    public boolean solve(int n){
-        if(n == 1) return true;
-        if(n%4 != 0) return false;
+        double answer = Math.log(n)/Math.log(4);
 
-        return solve(n/4);
+        return (answer == (int)answer);
     }
 }
