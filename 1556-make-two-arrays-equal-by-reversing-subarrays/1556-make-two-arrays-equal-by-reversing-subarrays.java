aa89693b7e838d84687146d31b661e0424d57825
Abd8603 <80663656+Abd8603@users.noreply.github.com>
2024-08-03 09:06:15 +0530

    Time: 1 ms (100%), Space: 44.4 MB (31.94%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
         int[] tar = new int[1001];
-        int[] ar = new int[1001];
-        int v;
         for(int i = 0; i < target.length;i++){
             tar[target[i]]++;
-            ar[arr[i]]++;
+            tar[arr[i]]--;
         }
         for(int i = 1; i < 1001;i++){
-            if(tar[i] != ar[i])
+            if(tar[i] != 0)
                 return false;
         }
         return true;
